25f3dade5b975853245e157e777061d559987ec3
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-05-23 19:07:49 +0400

    switch App js to class component

--- React Native/shop-list/App.js
+++ React Native/shop-list/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 import { RootDrawer } from './navigation/RootDrawer';
 import { loadFonts } from './styles/fonts';
@@ -7,25 +7,36 @@ import { Provider } from "react-redux";
 import store from './redux';
 import { AppLoading } from 'expo';
 
-export default function App() {
+export default class App extends Component {
 
-  const [loaded, setLoaded] = useState(false);
+  //const [loaded, setLoaded] = useState(false);
 
-
-  if(!loaded) {
-    return(
-      <AppLoading
-        startAsync={loadFonts}
-        onFinish={() => setLoaded(true)}
-        onError={() => console.log("Something Wrong")}
-      />
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false
+    };
   }
 
-  return (
-    
-    <Provider store={store}>
-        <RootDrawer />
-    </Provider>
-  );
-}
+  setLoaded = () => {
+    this.setState({ loaded: true });
+  };
+  
+  render() {
+    if(!this.state.loaded) {
+      return(
+        <AppLoading
+          startAsync={loadFonts}
+          onFinish={this.setLoaded}
+          onError={() => console.log("Something Wrong")}
+        />
+      );
+    }
+
+    return (
+      <Provider store={store}>
+          <RootDrawer />
+      </Provider>
+    );
+  }
+};
